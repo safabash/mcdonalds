65f4451b16f7c20ac6116df765d377ea8352edab
safabash <106664975+safabash@users.noreply.github.com>
2022-06-11 09:41:22 +0530

    Add files via upload

---
+++ McDonalds-main/script.js
@@ -0,0 +1,15 @@
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+function goup() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+
